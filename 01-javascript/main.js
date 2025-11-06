cef263f017b971d1e1383a225104bcbc219cce6f
José Pizarro <jose.pizarror2@userena.cl>
2025-11-05 21:22:23 -0300

    feat: dynamically load job listings from JSON and update filter functionality

--- 01-javascript/main.js
+++ 01-javascript/main.js
@@ -61,9 +61,10 @@ const filter = document.querySelector("#filter-location")
 
 const mensaje = document.querySelector("#filter-selected-value")
 
-const jobs = document.querySelectorAll(".job-listing-card")
+
 
 filter.addEventListener("change", () => {
+    const jobs = document.querySelectorAll(".job-listing-card")
     const selectedValue = filter.value;
 
     if (selectedValue) {
@@ -83,3 +84,29 @@ filter.addEventListener("change", () => {
     })
 
 });
+
+
+const container = document.querySelector(".jobs-listings")
+
+fetch("./data.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(jobs => {
+        jobs.forEach(job => {
+            const article = document.createElement('article')
+            article.className = "job-listing-card"
+            article.dataset.modalidad = job.data.modalidad
+            article.dataset.nivel = job.data.nivel
+            article.dataset.technology = job.data.technology
+
+            article.innerHTML = `<div>
+            <h3>${job.titulo}</h3>
+            <small>${job.empresa} | ${job.ubicacion}</small>
+            <p>${job.descripcion}</p>
+            
+            </div>
+            <button class="button-apply-job">Aplicar</button>`
+            container.appendChild(article)
+        })
+    });
